afde51942fe7fc0bb250e8e2403528da737f03bc
Kevin Herrera <kvnherrera.04@gmail.com>
2025-01-08 00:21:35 -0300

    algo6

--- app/config/DBConfig.py
+++ app/config/DBConfig.py
@@ -9,10 +9,11 @@ load_dotenv(dotenv_path='dataSensible.env')
 # contrasenia = "Kevinherrera.04"                                   SIN LAS VARIABLES .ENV
 # host = "localhost"  # o la IP del servidor de tu base de datos
 # db = "Kaminari"
-usuario = getenv('USUARIO')
-contrasenia = getenv('CONTRASENIA')
-host = getenv('HOST')
-db = getenv('DB')
+# usuario = getenv('USUARIO')
+# contrasenia = getenv('CONTRASENIA')
+# host = getenv('HOST')
+# db = getenv('DB')
+url_msql=getenv('URL_MYSQL')
 # Crear la URL de conexión para MySQL con mysql-connector-python
-engine = create_engine(f"mysql+mysqlconnector://{usuario}:{contrasenia}@{host}/{db}") # esto requiere de la libreria "mysql-connector-python"
+engine = create_engine(url=url_msql) # esto requiere de la libreria "mysql-connector-python"
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
